0c72613506a7e4adc62987075d670fe5d515761d
Jeremy-PR <jeremy.pelisse@gmail.com>
2024-12-04 15:00:36 +0100

    add : part 4 done

---
+++ index.php
@@ -0,0 +1,109 @@
+<?php
+
+// EXERCICE 1
+
+// function tryfunction(){
+//  return "true";
+// }
+// echo tryfunction()
+
+
+
+
+// EXERCICE 2
+
+
+// function test($blabla = "motherfucker"){
+//     return $blabla;
+// }
+
+// echo test ();
+
+
+
+
+// EXERCICE 3
+
+
+// function test($west = "wassup ", $east = "everybody"){
+//     return $west . $east;
+// }
+
+// echo test();
+
+
+// Exercice 4
+
+// function multiply (int $nombre1, int $nombre2){
+
+//     if ($nombre1 > $nombre2){
+//         return "Le premier nombre est plus grand";
+//     }else if($nombre1 < $nombre2){
+//         return "Le premier nombre est plus petit";
+//     } else if($nombre1 === $nombre2){
+//         return "Les deux nombres sont identiques";
+//     }      
+// }
+
+// echo multiply(57579,57579)
+
+
+// EXERCICE 5
+
+
+// function concatener($nombre, $chaine) {
+//     return $nombre . $chaine;
+// }
+
+
+// $nombre = 14647585867686;
+// $chaine = "€ ce que je veux sur mon compte";
+
+// echo concatener($nombre, $chaine);
+
+
+
+// // Exercice 6
+// function threeparameters(string $name, string $firstname, int $age){
+//     return "Wassup {$name} {$firstname} tu as {$age} anos";
+
+// }
+// echo threeparameters("Sabatier" , "Marion", 53)
+
+
+
+
+
+// Exercice 7
+
+// function ageGender(string $gender, int $age){
+//     if ($gender === 'homme') {
+//         if ($age >= 18) {
+//             return "Vous êtes un homme et vous êtes majeur";
+//         } else {
+//             return "Vous êtes un homme et vous êtes mineur";
+//         }
+//     } else if ($gender === 'femme') {
+//         if ($age >= 18) {
+//             return "Vous êtes une femme et vous êtes majeure";
+//         } else {
+//             return "Vous êtes une femme et vous êtes mineure";
+//         }
+//     }
+// }
+// echo ageGender("femme" , 4)
+
+
+
+// Exercice 8
+
+
+// function somme(int $age1, int $age2, int$age3){
+//     return $age1 + $age2 + $age3;
+// }
+
+// echo somme(23, 545, 2);
+
+
+?>
+
